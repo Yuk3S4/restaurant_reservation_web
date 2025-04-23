00861bf8fcc134d6d2741f795c0b4700d6a4ac1f
Daniel De la Torre <marco11.mendia@gmail.com>
2025-04-23 11:05:15 -0600

    first commit

--- src/app/shared/models/request/reservation-request.model.ts
+++ src/app/shared/models/request/reservation-request.model.ts
@@ -1,7 +1,6 @@
 export interface ReservationRequest {
-	restaurantId: number;	
-	reservationDate: string; 
+	restaurantId: number;
+	reservationDate: string;
 	numberOfPeople: number;
 	additionalInfo?: string;
-  }
-  
+}
